11b2dde6c4ca6042389df155d86aa3f601517be8
Stephanie Burgos <burgoss@google.com>
2022-09-22 15:41:58 -0400

    Add term 0 review challenges

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # te-coding-challenges
+
+Practice your knowledge of basic computer science concepts that we will continue to use in Term 1.
---
+++ term0-review.py
@@ -0,0 +1,65 @@
+#                      Term 0 Review 
+# Practice your knowledge of basic computer science concepts
+# that we will continue to use in Term 1.
+#
+#
+
+print("-------- Challenge 1 --------")
+# Challenge 1: Variables
+# Prompt: Instantiate a variable and print its
+# contents to the terminal.
+#
+
+print("-------- Challenge 2 --------")
+# Challenge 2: Loops
+# Prompt: Write a loop to print all of the odd
+# integers in the given nums[] list but none of the even
+# integers in the list.
+#
+nums = [1, 2, 4, 8, 11, 15, 16, 17, 18, 19, 21, 44, 49]
+
+
+print("-------- Challenge 3 --------")
+# Challenge 3: Function Definitions
+# Prompt:  Write a function, array_sum, that calculates
+# and prints the sum of the entries in a specified array.
+# For example, calling array_sum on the array [1,3,2,7,3]
+# should print 16 to the terminal.
+#  array_sum([1, 3, 2, 7, 3]) --> 16
+#  array_sum(nums) --> 225
+
+print("-------- Challenge 4 --------")
+# Challenge 4: Function Calling
+# Prompt: Call the function defined in challenge 3 on an array.
+# of 5 or more numbers. Make sure it works
+#
+
+print("-------- Challenge 5 --------")
+# Challenge 5:
+# Prompt: Instantiate one variable that will store a string, name.
+# Instantiate another variable that will store a number, age.
+# Print the following string by accessing your two variables:
+# 'I am _name_ and I am _age_ years old'
+#
+
+print("-------- Challenge 6 --------")
+# Challenge 6:
+# Prompt: Write a Python function, compute_area, that will accept
+# the base and height of a triangle as parameters and return
+# the computed area. 
+# For example, calling compute_area with the arguments 3 (height) and 
+# 4 (base), the function should return 6:
+# compute_area(3, 4) --> 6
+#
+#
+
+
+
+print("-------- Challenge 7 --------")
+# Challenge 7:
+# Prompt: Write a Python function, max_min, that will accept
+# a list of integers as a parameter and print the largest
+# and smallest integers.
+# max_min(nums) --> 1    49
+# max_min([1, 3, 2, 7, 3]) --> 1, 7 
+#

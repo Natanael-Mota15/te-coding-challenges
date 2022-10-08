0d46d0f99bfd83b5a2184981548e61be51c843fd
Natanael-Mota15 <108821447+Natanael-Mota15@users.noreply.github.com>
2022-10-07 22:59:34 -0400

    Completed

--- term0-review.py
+++ term0-review.py
@@ -4,21 +4,27 @@
 #
 #
 
+import numbers
+
+
 print("-------- Challenge 1 --------")
 # Challenge 1: Variables
 # Prompt: Instantiate a variable and print its
 # contents to the terminal.
 #
-
+greeting = ("Hello")
+print(greeting)
 print("-------- Challenge 2 --------")
 # Challenge 2: Loops
 # Prompt: Write a loop to print all of the odd
 # integers in the given nums[] list but none of the even
 # integers in the list.
 #
+
 nums = [1, 2, 4, 8, 11, 15, 16, 17, 18, 19, 21, 44, 49]
-
-
+for i in nums:
+    if (i % 2 != 0):
+       print(i)
 print("-------- Challenge 3 --------")
 # Challenge 3: Function Definitions
 # Prompt:  Write a function, array_sum, that calculates
@@ -28,11 +34,25 @@ print("-------- Challenge 3 --------")
 #  array_sum([1, 3, 2, 7, 3]) --> 16
 #  array_sum(nums) --> 225
 
+def array_sum():
+    listnum = []
+    
+    inputnum = input("what numbers you want to add")
+    if (inputnum != "done"):
+        inputnum = input("what numbers you want to add")
+        listnum.append(int(inputnum))
+    elif inputnum == "done":
+        for numbe in range(0, len(listnum)):
+            total =  total + listnum[numbe]
+ 
+
+
 print("-------- Challenge 4 --------")
 # Challenge 4: Function Calling
 # Prompt: Call the function defined in challenge 3 on an array.
 # of 5 or more numbers. Make sure it works
 #
+array_sum()
 
 print("-------- Challenge 5 --------")
 # Challenge 5:
@@ -41,7 +61,10 @@ print("-------- Challenge 5 --------")
 # Print the following string by accessing your two variables:
 # 'I am _name_ and I am _age_ years old'
 #
+name = "Natanael"
+age = "15"
 
+print(f"I am {name} and I am {age} years old.")
 print("-------- Challenge 6 --------")
 # Challenge 6:
 # Prompt: Write a Python function, compute_area, that will accept
@@ -52,9 +75,12 @@ print("-------- Challenge 6 --------")
 # compute_area(3, 4) --> 6
 #
 #
-
-
-
+base = input("what is the base")
+height = input("What is the height")
+base1 = int(base)
+height2 = int(height)
+answer = (base1 * height2)/2
+print(answer)
 print("-------- Challenge 7 --------")
 # Challenge 7:
 # Prompt: Write a Python function, max_min, that will accept
@@ -63,3 +89,13 @@ print("-------- Challenge 7 --------")
 # max_min(nums) --> 1    49
 # max_min([1, 3, 2, 7, 3]) --> 1, 7 
 #
+
+numlist = []
+nu = input("What numbers you want to sort, type done when done")
+if nu == "done":
+    numlist.sort()
+    print(numlist[0])
+    numb = len(numlist)
+    print(numlist[numb])
+else:
+    nu = input("What numbers you want to sort, type done when done")
